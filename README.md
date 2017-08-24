e49b00260f391a02b5354452eca0a0bf4039e337
hanford <jackhanford@gmail.com>
2017-08-24 09:52:10 -0700

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# is-webapp
+
+> Detect if website was launched from mobile phone homescreen
+
+
+## Install
+
+```
+$ npm install --save is-webapp
+```
+
+
+## Usage
+
+```js
+const isWebApp = require('is-webapp')
+
+isWebApp()
+//=> true || false
+```
+
+## License
+
+MIT © [Jack Hanford](https://jackhanford.com)
---
+++ index.js
@@ -0,0 +1,16 @@
+const window = require('global/window')
+
+module.exports = IsWebApp
+
+function IsWebApp () {
+  if (window.navigator && window.navigator.standalone === true) {
+    // is loaded from iOS home screen
+    return true
+  } else if (window.matchMedia('(display-mode: standalone)').matches) {
+    // is loaded from android home screen
+    return true
+  } else {
+    return false
+  }
+}
+
