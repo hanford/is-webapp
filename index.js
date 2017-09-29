3ce096d937cbfcd424cc6b6d78a880df3e31383d
hanford <jackhanford@gmail.com>
2017-09-29 16:42:33 -0700

    window.matchMedia check

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function IsWebApp () {
   if (window.navigator && window.navigator.standalone === true) {
     // is loaded from iOS home screen
     return true
-  } else if (window.matchMedia('(display-mode: standalone)').matches) {
+  } else if (window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) {
     // is loaded from android home screen
     return true
   } else {
